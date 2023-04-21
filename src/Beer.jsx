29b6632b9e19e0e13cf0d27df2ad3c1b19d3b1c5
ConnorMcGehee <connorjames050@gmail.com>
2023-04-20 20:40:55 -0500

    Added functionality

---
+++ src/Beer.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Beer = ({ name, description, image }) => {
+    const [liked, setLike] = useState(false);
+    return (
+        <>
+            <img width={100} src={image}></img>
+            <h2>{name}</h2>
+            <p>{description}</p>
+            <button onClick={() => setLike((liked) => !liked)} className={`liked${liked.toString()}`}>Like</button>
+            <br />
+        </>
+    );
+}
+
+export default Beer;
